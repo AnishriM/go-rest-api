5d7a5a3a73ada29b3d095ffa2ffa282d8fc3eec6
Sagar Mhantati <34499571+AnishriM@users.noreply.github.com>
2023-02-04 13:14:28 +0000

    Added health-check endpoint

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,9 +1,27 @@
 package http
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 // Handler - stores pointer to our comment service
-type Handler struct{}
+type Handler struct {
+	Router *mux.Router
+}
 
 // NewHandler - returns a pointer to a Handler
 func NewHandler() *Handler {
 	return &Handler{}
+}
+
+// SetupRoutes - sets up all the routes for out application
+func (h *Handler) SetupRoutes() {
+	fmt.Println("Setting up the routes.")
+	h.Router = mux.NewRouter()
+	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "I'm alive!")
+	})
 }
